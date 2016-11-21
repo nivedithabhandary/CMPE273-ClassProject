7949a93b5a9f76a1e19b3d8cc6ad7cc532076a7e
skulai <shruthi.kulai29@gmail.com>
2016-11-21 01:30:54 -0800

    Auto complete for single field

--- runserver.py
+++ runserver.py
@@ -4,31 +4,5 @@
 
 from flask import Flask, render_template
 from tripplanner import app
-#import os
-#import jinja2
-
-#app = Flask(__name__)
-#app.jinja_loader = jinja2.FileSystemLoader('/tripplanner/templates')
-
-#tbd for / routing
-@app.route('/')
-def hello_world():
-    return 'Hello, User!'
-
-#the first page to login to the application
-#add authentication methods
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-#main page of the application
-@app.route('/home')
-def home():
-    return render_template('home.html')    
-
-#Locations page 
-@app.route('/locations')
-def locations():
-    return render_template('locations.html')    
 
 app.run(debug=True)
---
+++ tripplanner/static/js/autocomplete.js
@@ -0,0 +1,7 @@
+(function ($) {
+    "use strict"; // Start of use strict
+    $(document).ready(function () {	
+		 var input = document.getElementById('start');
+	     var autocomplete = new google.maps.places.Autocomplete(input);
+	});	
+})(jQuery); // End of use strict    
