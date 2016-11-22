6460ac8c12d11df2cb3a25e0e0ae0a3c2a6ea1ab
skulai <shruthi.kulai29@gmail.com>
2016-11-22 01:08:17 -0800

    autocomplete

--- tripplanner/static/js/autocomplete.js
+++ tripplanner/static/js/autocomplete.js
@@ -1,7 +1,27 @@
-(function ($) {
+/* (function ($) {
     "use strict"; // Start of use strict
     $(document).ready(function () {	
 		 var input = document.getElementById('start');
 	     var autocomplete = new google.maps.places.Autocomplete(input);
 	});	
-})(jQuery); // End of use strict    
+})(jQuery); // End of use strict */ 
+
+(function ($) {
+    "use strict"; // Start of use strict
+    $(document).ready(function () {
+		$("#tripplan input[type=text]").each(function() { 	
+			var input = $(this).attr("id");
+			console.log(input);
+			var autocomplete = new google.maps.places.Autocomplete(document.getElementById(input));
+		});
+	});		
+})(jQuery);   
+
+$("#addlocation").on('click',function (evt) {		
+		evt.preventDefault();
+		$("#tripplan input[type=text]").each(function() { 
+			var currentInp = $(this).attr("id");
+			console.log(currentInp);
+			var placeBox = new google.maps.places.Autocomplete(document.getElementById(currentInp));
+		});		
+});
