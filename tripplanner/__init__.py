a9a08a8eea9da79ee46cfa88586142fbe056ce18
Niveditha Bhandary <niveditha.bhandary@sjsu.edu>
2016-11-24 16:19:19 -0800

    Modified init

--- tripplanner/__init__.py
+++ tripplanner/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from .views.sample import sample
 from .views.planner import planner
+from .views.oauth import oauth
 
 app = Flask(__name__)
 app.register_blueprint(sample)
 app.register_blueprint(planner)
+app.register_blueprint(oauth)
