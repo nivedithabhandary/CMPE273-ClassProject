a68e341158774552e563cf2a01300b30986a1122
Jaya Kirtani <jaya.kirtani@sjsu.edu>
2016-11-26 12:23:04 -0800

    Added delete functionality for dynamically added via locations in Main Page.

--- tripplanner/static/js/autocomplete.js
+++ tripplanner/static/js/autocomplete.js
@@ -1,6 +1,14 @@
 function GetDynamicTextBox(count) {
     "use strict";
-    return '<div class="col-sm-8 col-sm-offset-3"><div class="form-group"><input type="text" class="form-control" id="vialocation' + count + '" name="vialocation" placeholder="via Location"></div></div>';
+    //    return '<div class="col-xs-8 col-sm-offset-3"><div class="form-group"><div class="input-group input-group-unstyled"><input type="text" class="form-control" id="vialocation' + count + '" name="vialocation" placeholder="via Location"><span class="input-group-addon"><a id="remove" onclick="removeTextbox()"><i class="fa fa-times fa-1x" style="color:white;"></i></a></span></div></div></div>';
+
+    return '<div class="form-group"><div class="col-xs-8 col-sm-offset-3"><input type="text" class="form-control" id="vialocation' + count + '" name="vialocation" placeholder="via Location"></div><div class="col-xs-1"><a id="remove" title ="Delete via Location" onclick="removeTextbox(this)"><i class="fa fa-times fa-1x" style="color:white;"></i></a></div></div>';
+}
+
+function removeTextbox(elem) {
+    console.log("Inside remove textbox function");
+    //Remove Textbox on icon click
+    $(elem).parent().parent().remove();
 }
 
 (function ($) {
