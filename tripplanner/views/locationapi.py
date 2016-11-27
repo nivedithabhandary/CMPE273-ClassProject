091d4f36673d56be66e4b6ab92fd482db0117eb5
Niveditha Bhandary <niveditha.bhandary@sjsu.edu>
2016-11-27 11:38:33 -0800

    Modified Location api's

--- tripplanner/views/locationapi.py
+++ tripplanner/views/locationapi.py
@@ -11,11 +11,13 @@ def set_mongo_handler(mongo_handle):
     global g_mongo_handle
     g_mongo_handle = mongo_handle
 
+# Post location info
 @locationapi.route('/locations', methods=['POST'])
 def add_locations():
   global g_mongo_handle
   location = g_mongo_handle.db.locations
 
+  email = request.json['email']
   name = request.json['name']
   address = request.json['address']
   city = request.json['city']
@@ -31,52 +33,57 @@ def add_locations():
    }
 
 
-  location_id = location.insert({'name': name, 'address':address,
+  location_id = location.insert({'email': email, 'name': name, 'address':address,
   'city':city, 'state': state, 'zipcode':zipcode, 'coordinate':coordinate})
 
   new_entry = location.find_one({'_id': location_id })
 
-  output = {'id':str(new_entry['_id']), 'name' : new_entry['name'],
-  'address':new_entry['address'],'city':new_entry['city'],
-  'state': new_entry['state'], 'zipcode': new_entry['zipcode'],
-  'coordinate':new_entry['coordinate'] }
+  output = {'id':str(new_entry['_id']), 'email': new_entry['email'],
+  'name' : new_entry['name'],'address':new_entry['address'],
+  'city':new_entry['city'],'state': new_entry['state'],
+  'zipcode': new_entry['zipcode'],'coordinate':new_entry['coordinate'] }
 
   return json.dumps(output)
 
-@locationapi.route('/locations/<name>', methods=['GET'])
-def get_location(name):
+# Get all locations with email
+@locationapi.route('/locations/<email>', methods=['GET'])
+def get_all_locations(email):
   global g_mongo_handle
   location = g_mongo_handle.db.locations
-  found = location.find_one({'name' : name})
+  found = location.find_one({'email' : email})
   if found:
-    output = {'id':str(found['_id']), 'name' : found['name'],
-    'address':found['address'],'city':found['city'],
-    'state': found['state'], 'zipcode': found['zipcode'],
-    'coordinate':found['coordinate']}
+      output = []
+      for s in location.find({'email' : email}):
+          output.append({'id':str(s['_id']), 'email' : s['email'],
+          'name' : s['name'],
+          'address':s['address'],'city':s['city'],
+          'state': s['state'], 'zipcode': s['zipcode'],
+          'coordinate':s['coordinate']})
   else:
-    output = "Error: No such saved location name found!"
+    output = "Error: No such saved email found!"
   return jsonify({'result' : output})
 
-@locationapi.route('/locations/<name>', methods=['PUT'])
-def modify_location(name):
+@locationapi.route('/locations/<email>', methods=['PUT'])
+def modify_location(email):
   global g_mongo_handle
   location = g_mongo_handle.db.locations
-  found = location.find_one({'name' : name})
-  new_name = request.json['name']
+  old_name = request.json['old_name']
+  new_name = request.json['new_name']
+  found = location.find_one({'$and':[{'email' : email}, {'name':old_name}]})
   if found:
-      location.update({'name':name}, {"$set":{"name": new_name}})
+      location.update({'$and':[{'email' : email}, {'name':old_name}]}, {"$set":{"name": new_name}})
       output = "Update Success"
   else:
       output = "Error: No such saved location name found!"
   return jsonify({'result' : output})
 
-@locationapi.route('/locations/<name>', methods=['DELETE'])
-def delete_location(name):
+@locationapi.route('/locations/<email>', methods=['DELETE'])
+def delete_location(email):
   global g_mongo_handle
   location = g_mongo_handle.db.locations
-  found = location.find_one({'name' : name})
+  found = location.find_one({'email' : email})
   if found:
-      location.remove({'name':name})
+      location.remove({'email':email})
       output = "Delete Success"
   else:
       output = "Error: No such saved location name found!"
