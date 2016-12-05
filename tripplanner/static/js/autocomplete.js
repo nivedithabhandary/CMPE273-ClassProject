11d24b44b5d948fa133a9e5c80010c2f20b1639c
skulai <shruthi.kulai29@gmail.com>
2016-12-05 11:59:15 -0800

    "input validation on index page"

--- tripplanner/static/js/autocomplete.js
+++ tripplanner/static/js/autocomplete.js
@@ -2,7 +2,7 @@ function GetDynamicTextBox(count) {
     "use strict";
     //    return '<div class="col-xs-8 col-sm-offset-3"><div class="form-group"><div class="input-group input-group-unstyled"><input type="text" class="form-control" id="vialocation' + count + '" name="vialocation" placeholder="via Location"><span class="input-group-addon"><a id="remove" onclick="removeTextbox()"><i class="fa fa-times fa-1x" style="color:white;"></i></a></span></div></div></div>';
 
-    return '<div class="form-group"><div class="col-xs-10 col-sm-8 col-sm-offset-3"><input type="text" class="form-control" id="vialocation' + count + '" name="vialocation" placeholder="via Location"></div><div class="col-xs-1"><a id="remove" title ="Delete via Location" onclick="removeTextbox(this)"><i class="fa fa-times-circle-o fa-1x" style="color:white;"></i></a></div></div>';
+    return '<div class="form-group"><div class="col-xs-10 col-sm-8 col-sm-offset-3"><input type="text" class="form-control" id="vialocation' + count + '" name="vialocation" placeholder="via Location" onchange="validateData(this)"></div><div class="col-xs-1"><a id="remove" title ="Delete via Location" onclick="removeTextbox(this)"><i class="fa fa-times-circle-o fa-1x" style="color:white;"></i></a></div></div>';
 
 }
 
@@ -16,15 +16,18 @@ function removeTextbox(elem) {
     "use strict"; // Start of use strict
     $(document).ready(function () {
         var count = 0;
-
+		
         //Adds dynamic textbox on plus icon click
         $('#addlocation').click(function () {
             count++;
-            //$('#vialocation').append('<div class="col-sm-8 col-sm-offset-3"><div class="form-group"><input type="text" class="form-control" id="vialocation" name="vialocation" placeholder="via Location"></div></div>');
+            //$('#vialocation').append('<div class="col-sm-8 col-sm-offset-3"><div class="form-group"><input type="text" class="form-control" id="vialocation" name="vialocation" onchange="validateData(this)" placeholder="via Location"></div></div>');
             $('#vialocation').append(GetDynamicTextBox(count));
             var newEl = document.getElementById('vialocation' + count);
             var autocomplete = new google.maps.places.Autocomplete(newEl);
             console.log("Added Location Textbox");
+			google.maps.event.addListener(autocomplete, 'place_changed', function() {
+				validateData(newEl);
+			});
         });
 
 
@@ -32,6 +35,36 @@ function removeTextbox(elem) {
             var input = $(this).attr("id");
             console.log(input);
             var autocomplete = new google.maps.places.Autocomplete(document.getElementById(input));
+			google.maps.event.addListener(autocomplete, 'place_changed', function() {
+				validateData(document.getElementById(input));
+			});
+			
         });
+		
     });
 })(jQuery);
+
+//validate input to be one from the autocomplete list
+function validateData(elem) {
+	var validdata = false;
+	var service = new google.maps.places.AutocompleteService();
+	service.getPlacePredictions({input: $(elem).val()},function(predictions, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			for (var i = 0; i < predictions.length; i++) {
+				if ((predictions[i].description) === ($(elem).val())) {
+				validdata = true;
+				$(elem).parent().parent().removeClass("has-feedback");
+				$(elem).parent().children().remove(".glyphicon");
+				$(elem).removeClass("incorrectdata");
+				break;
+				}
+			}
+			if (!validdata) {
+			console.log("Correct the Data");
+			$(elem).parent().parent().addClass("has-feedback");
+			$(elem).after('<span class="glyphicon glyphicon-remove form-control-feedback" style="color:red"></span>');
+			$(elem).addClass("incorrectdata");
+			}
+        }
+	});	
+}
