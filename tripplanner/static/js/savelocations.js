a1878955ba5d3777e08722866a5023f63699e5de
rahulVudutala <rahulbharadwaj.v@gmail.com>
2016-12-07 01:33:18 -0800

    added save, update, delete functionalities

--- tripplanner/static/js/savelocations.js
+++ tripplanner/static/js/savelocations.js
@@ -1,12 +1,63 @@
+function loadPrevLocations(useremail) {
+	var result = useremail.slice(1, -1);
+	
+	var apicall = "/locations/" + result;
+        console.log("Location API Call :" + apicall);
+        var userLocations;
+        var locationsdata;
+        $.get(apicall).done(function (data) {
+            console.log("Output from my server :" + data);
+            var output = JSON.parse(data);
+			var count = 0;
+            if (output.length > 0) {
+                locationsdata = output[0].locations
+                    console.log(locationsdata);
+                    console.log(locationsdata.locations);
+                userLocations = Object.keys(locationsdata);
+                //console.log(userLocations);
+                // Add User saved Locations to the datalist
+                userLocations.forEach(function (location) {
+                    //console.log(location);
+					console.log("count",location);
+							if(count == 0) {
+								document.getElementById("loc1").innerHTML = location;
+								document.getElementById("loc1").value = location;
+								document.getElementById("loc1addr").value = locationsdata[location];
+							} else if(count == 1) {
+								document.getElementById("loc2").innerHTML = location;
+								document.getElementById("loc2").value = location;
+								document.getElementById("loc2addr").value = locationsdata[location];
+							} else if(count == 2) {
+								document.getElementById("loc3").innerHTML = location;
+								document.getElementById("loc3").value = location;
+								document.getElementById("loc3addr").value = locationsdata[location];
+							} else if(count == 3) {
+								document.getElementById("loc4").innerHTML = location;
+								document.getElementById("loc4").value = location;
+								document.getElementById("loc4addr").value = locationsdata[location];
+							}	
+							count++;
+                    //$("#locationsList").append('<option id="userloc" value="' + locationsdata[location] + '">' + location + '</option>')
+                });
+            }
+        });
+}
+
 (function ($) {
 "use strict"; // Start of use strict
 
     $(document).ready(function () {
+		//document.getElementById("deleteLocationDiv").style.visibility='hidden';
+		var mycookie = document.cookie;
+		var useremail = mycookie.split('=')[1];
+		console.log("useremail :" + useremail);
+		loadPrevLocations(useremail);
+			
 		$('#savelocations').click(function () {
 			console.log("Inside plan trip button function");
-	        var mycookie = document.cookie;
-			var useremail = mycookie.split('=')[1];
-			console.log("useremail :" + useremail);
+			console.log("in function");
+			
+	        
 			//Fetch the start , end and via locations
 			var nameOfAddress = $("#nameOfAddress").val();
 			var location = $("#address").val();
@@ -27,8 +78,8 @@
 			console.log(locationMap);
 			var inData = [{"email":useremail,"locations":locationList}];
 			console.log(inData);
-			var output = $.get("https://jsonplaceholder.typicode.com/posts/1");
-			console.log("Output from server :" + JSON.stringify(output) + ":" + output.readyState);
+			//var output = $.get("https://jsonplaceholder.typicode.com/posts/1");
+			//console.log("Output from server :" + JSON.stringify(output) + ":" + output.readyState);
 			$.ajax({
 				 type: "POST",
 				 url: "http://localhost:5000/locations",
@@ -38,22 +89,49 @@
 				 success: function (data, status, jqXHR) {
 					  // do something
 					  console.log("success");
+					  //$('#containerDiv').load(window.location.href +  ' #containerDiv');
+					  loadPrevLocations(useremail);
+					  document.getElementById("nameOfAddress").value = "";
+					document.getElementById("address").value = "";
+					document.getElementById("savelocations").value = "Save Locations";
+					document.getElementById("deleteLocationDiv").style.visibility='hidden';
                 },
 				 error: function (jqXHR, status) {
 						console.log(status);
 			}});
-			//var postoutput = $.post("http://localhost:5000/locations", {
-            //    email: 'rahulbharadwaj.v@gmail.com',
-            //    name: 'home',
-            //    address: '1334 The Alameda, San Jose, CA, United States'
-            //});
-			//console.postoutput("Post output:" + JSON.stringify(postoutput));
+			
+			});
+			$('#deletelocation').click(function () {
+			console.log("Inside plan trip button function");
+			console.log("in function");
+			//Fetch the start , end and via locations
+			var nameOfAddress = $("#nameOfAddress").val();
+			var location = $("#address").val();
 
-			//Dummy get call to server api
-			//$.get("hello").done(function (data) {
-			//      console.log("Output from my server :" + data);
-			//});
-
+			console.log("nameOfAddress: " + nameOfAddress);
+			
+			var inData = [{"email":useremail,"name":nameOfAddress}];
+			console.log(inData);
+			//var output = $.get("https://jsonplaceholder.typicode.com/posts/1");
+			//console.log("Output from server :" + JSON.stringify(output) + ":" + output.readyState);
+			$.ajax({
+				 type: "DELETE",
+				 url: "http://localhost:5000/locations/".concat(useremail),
+				 data: JSON.stringify(inData),
+				 contentType: "application/json",
+				 dataType: "json",
+				 success: function (data, status, jqXHR) {
+						console.log(data);
+					  document.getElementById("nameOfAddress").value = "";
+					document.getElementById("address").value = "";
+					document.getElementById("savelocations").value = "Save Locations";
+					document.getElementById("deleteLocationDiv").style.visibility='hidden';
+					$('#locationsDiv').load(window.location.href +  ' #locationsDiv');
+					loadPrevLocations(useremail);
+                },
+				 error: function (jqXHR, status) {
+						console.log(status);
+			}});
 			});
 	});
 })(jQuery);
